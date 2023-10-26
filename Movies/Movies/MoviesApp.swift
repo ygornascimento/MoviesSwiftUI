0b184d0c0867c6b8ea6eabb69a95d7e5b79ee4b7
Ygor Nascimento <ygor.nascimento@outlook.com>
2023-10-26 09:23:55 -0300

    First Commit

---
+++ Movies/Movies/MoviesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoviesApp.swift
+//  Movies
+//
+//  Created by Ygor Nascimento on 25/10/23.
+//
+
+import SwiftUI
+
+@main
+struct MoviesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
